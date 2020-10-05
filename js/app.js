166960e97a895ab7cff2aeaa5a9e9deffcdc1721
nickmackenzie <nick-m@live.ca>
2020-10-05 09:54:07 -0400

    push

--- js/app.js
+++ js/app.js
@@ -19,6 +19,7 @@ let playerInput = document.getElementById("player-guess");
 let guessBtn = document.getElementById("guess-btn");
 let blankWord = document.getElementById("word");
 let waterfall = document.getElementById("waterfall");
+let playerReset = document.getElementById("player");
 let playerMover = 0;
 let playerGuess = [];
 let playerLife = wordArray.length;
@@ -27,8 +28,9 @@ let correctAnswerArray = [];
 let filledArray = [];
 let blankey = document.createElement("div");
 let scream = new Audio("/sounds/scream.mp3");
-// let playerCanoe = { c1: "<img src='img/person.gif'>" }
-let playerCanoe = { c1: "<img src='img/giphy.gif'>" }
+let playerCanoe = { c1: "<img src='img/person.gif'>" }
+let playerTile = document.createElement("div");
+
 
 
 playBtn.addEventListener("click", playButton);
@@ -41,12 +43,49 @@ function playerMoverFun() {
 
 function playButton() {
   playerArray = [];
+
   wordChooser();
   wordSpliter();
   tileMaker();
   blankMaker();
   blankey.innerHTML = filledArray.join("");
   word.append(blankey);
+}
+
+
+function wordChooser() {
+  waterfall.style.display = "block";
+  river.textContent = "";
+  let wordChooser = Math.floor(Math.random() * 9);
+  wordChoosen = arrayWords[wordChooser];
+  playerLife = wordChoosen.length;
+  return wordChoosen;
+}
+
+function wordSpliter() {
+  wordArray = wordChoosen.split("");
+  console.log(wordArray);
+}
+
+function tileMaker() {
+  tileLength = wordChoosen.length;
+  let tileString = tileLength.toString();
+  wordArray.forEach(function (word, idx) {
+    let newTile = document.createElement("div");
+    newTile.className = "water";
+    newTile.id = idx;
+    newTile.innerHTML = "<img src='img/watertile.png'>";
+    river.append(newTile);
+    if (newTile.id === "0") { // I could make (array.length - 1) the water fall tile from this. Get rid of the player tile as 
+      playerTile = newTile;  // Its hard to control and it will be easier to control.
+      playerTile.className = "player";
+      playerTile.style.width = "25px";
+      playerTile.style.height = "25px";
+      playerTile.style.position = "absolute";
+      playerTile.id = "player";
+      playerTile.innerHTML = playerCanoe.c1;
+    }
+  });
 }
 
 function blankMaker() {
@@ -97,39 +136,6 @@ function inputCheck() {
   }
 }
 
-function wordSpliter() {
-  wordArray = wordChoosen.split("");
-  console.log(wordArray);
-}
 
-function tileMaker() {
-  tileLength = wordChoosen.length;
-  let tileString = tileLength.toString();
-  wordArray.forEach(function (word, idx) {
-    let newTile = document.createElement("div");
-    newTile.className = "water";
-    newTile.id = idx;
-    newTile.innerHTML = "<img src='img/watertile.png'>";
-    river.append(newTile);
-    if (newTile.id === "0") {
-      let playerTile = document.createElement("div");
-      playerTile = newTile;
-      playerTile.className = "player slide-in-left";
-      playerTile.style.width = "25px";
-      playerTile.style.height = "25px";
-      playerTile.style.position = "absolute";
-      playerTile.style.marginTop = "50px";
-      playerTile.id = "player";
-      playerTile.innerHTML = playerCanoe.c1;
-    }
-  });
-}
 
-function wordChooser() {
-  waterfall.style.display = "block";
-  river.textContent = "";
-  let wordChooser = Math.floor(Math.random() * 9);
-  wordChoosen = arrayWords[wordChooser];
-  playerLife = wordChoosen.length;
-  return wordChoosen;
-}
+
